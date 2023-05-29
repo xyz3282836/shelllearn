e2ed669eff406352fdb6460f1d30d6a3664ce506
liuruizhou <liuruizhou@bilibili.com>
2023-05-29 19:52:36 +0800

    add trap

--- func.sh
+++ func.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# trap "echo 'xxoo' " HUP INT QUIT TSTP
 function name1() {
     echo $1
     echo $2
@@ -8,3 +8,9 @@ function name1() {
 name1 1 2
 # 一般无返回值函数执行返回0
 echo $?
+while true; do
+    sleep 2
+    echo getpid
+done
+
+
