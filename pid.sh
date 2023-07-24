7cc13bc357ae0a517cb56dc61dcb7d70577fac13
liuruizhou <liuruizhou@bilibili.com>
2023-07-24 13:11:18 +0800

    add

--- pid.sh
+++ pid.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # set -xueo pipefail
 
-echo $1
-echo $2
+# echo $1
+# echo $2
 
 echo "$@"
 echo "main pid is $$"
 
-# . 表示引入文件
-# . ./subpid.sh
+# . 表示引入文件或者source
+# . ./subpid.sh or source ./subpid.sh
 
-./subpid.sh &
+. ./subpid.sh &
 # 等待后台进程完成
 wait $!
-echo "last bg pid"
+echo "last bg pid $!"
 echo $!
